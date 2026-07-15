5db4de665c5e792037911d78532d76955b7cd63d
KendiAstro23 <litalakendy975@gmail.com>
2026-07-15 23:47:42 +0300

    Finished the test cases, version 3

--- submission/01.sh
+++ submission/01.sh
@@ -1,2 +1,7 @@
 # Create a wallet with the name "btrustwallet".
-bitcoin-cli -named createwallet wallet_name=btrustwallet descriptors=true
+#!/usr/bin/env bash
+
+bitcoin-cli -regtest -named createwallet \
+  wallet_name=btrustwallet \
+  descriptors=true \
+| grep -o '"name": *"[^"]*"' | cut -d'"' -f4
